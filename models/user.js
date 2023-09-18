d0a8d73c7a4479d4077e6f12213b649bca3dbfd5
Nathan Berger <nberger101@gmail.com>
2023-09-18 15:08:10 -0400

    added register method

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config.js");
+const db = require("../db.js");
+
 /** User of the site. */
 
 class User {
@@ -9,6 +13,14 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone, join_at)
+      VALUES ($1, $2, $3, $4, $5, current_timestamp)
+      RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]);
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
