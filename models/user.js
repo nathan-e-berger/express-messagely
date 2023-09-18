a12f7f43daf9d6de4251a2a721baa041afff9515
Nathan Berger <nberger101@gmail.com>
2023-09-18 18:43:47 -0400

    added messagesFrom() and messagesTo()

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@
 const bcrypt = require("bcrypt");
 const { BCRYPT_WORK_FACTOR } = require("../config.js");
 const db = require("../db.js");
-const {NotFoundError, UnauthorizedError} = require("../expressError.js")
+const { NotFoundError, UnauthorizedError } = require("../expressError.js");
 
 /** User of the site. */
 
@@ -16,8 +16,14 @@ class User {
   static async register({ username, password, first_name, last_name, phone }) {
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
     const result = await db.query(
-      `INSERT INTO users (username, password, first_name, last_name, phone, join_at)
-      VALUES ($1, $2, $3, $4, $5, current_timestamp)
+      `INSERT INTO users (username,
+                          password,
+                          first_name,
+                          ast_name,
+                          phone,
+                          last_login_at,
+                          join_at)
+      VALUES ($1, $2, $3, $4, $5, current_timestamp,)
       RETURNING username, password, first_name, last_name, phone`,
       [username, hashedPassword, first_name, last_name, phone]);
 
@@ -28,17 +34,17 @@ class User {
 
   static async authenticate(username, password) {
     const result = await db.query(
-    `SELECT password
+      `SELECT password
     FROM users
     WHERE username = $1`,
-    [username]);
+      [username]);
     const user = result.rows[0];
 
     if (!user) {
-      throw new NotFoundError("Username not found")
+      throw new NotFoundError("Username not found");
     }
 
-    return await bcrypt.compare(password, user.password)
+    return await bcrypt.compare(password, user.password);
   }
 
   /** Update last_login_at for user */
@@ -54,7 +60,7 @@ class User {
     const user = result.rows[0];
 
     if (!user) {
-      throw new NotFoundError("Username not found")
+      throw new NotFoundError("Username not found");
     }
   }
 
@@ -62,6 +68,10 @@ class User {
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const result = await db.query(
+      `SELECT username, first_name, last_name
+      FROM users;`);
+    return result.rows;
   }
 
   /** Get: get user by username
@@ -74,6 +84,18 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const result = await db.query(
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at
+      FROM users
+      WHERE username = $1`,
+      [username]
+    );
+    const user = result.rows[0];
+
+    if (!user) {
+      throw new NotFoundError("Invalid username!");
+    }
+    return user;
   }
 
   /** Return messages from this user.
@@ -85,6 +107,30 @@ class User {
    */
 
   static async messagesFrom(username) {
+    const userResults = await db.query(
+      `SELECT username, first_name, last_name, phone
+      FROM users
+      WHERE username = $1`,
+      [username]
+    );
+    const user = userResults.rows[0];
+
+    if (!user) {
+      throw new NotFoundError("Invalid username!");
+    }
+
+    const messageResults = await db.query(
+      `SELECT id, body, sent_at, read_at
+      FROM messages
+      WHERE to_username = $1`,
+      [username]
+    );
+    const messages = messageResults.rows;
+
+
+    messages.map(m => m.to_user = user);
+
+    return messages;
   }
 
   /** Return messages to this user.
@@ -96,6 +142,30 @@ class User {
    */
 
   static async messagesTo(username) {
+
+    const messageResults = await db.query(
+      `SELECT id, body, sent_at, read_at
+      FROM messages
+      WHERE to_username = $1`,
+      [username]
+    );
+    const messages = messageResults.rows;
+
+    messages.map(m => {
+      return m.
+    });
+
+    const userResults = await db.query(
+      `SELECT username, first_name, last_name, phone
+        FROM users
+        WHERE username = $1`,
+      [username]
+    );
+    const user = userResults.rows[0];
+
+    messages.map(m => m.from_user = user);
+
+    return messages;
   }
 }
 
