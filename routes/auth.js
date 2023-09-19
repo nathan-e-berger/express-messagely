e4f8cff1dd27e4f202262e3582b489fa06976514
Christopher Alley <calleyviola@gmail.com>
2023-09-19 13:20:26 -0400

    Change auth routes to return json and _token

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const { authenticateJWT } = require("../middleware/auth");
 const { SECRET_KEY } = require("../config.js");
 const { UnauthorizedError } = require("../expressError");
 const User = require("../models/user");
@@ -16,9 +15,9 @@ router.post("/login", async function (req, res, next) {
   if (!(await User.authenticate(username, password))) {
     throw new UnauthorizedError("Nah ah ah");
   }
-  const payload = username;
-  const token = jwt.sign(payload, SECRET_KEY);
-  return res.send({ token });
+  const payload = { username };
+  const _token = jwt.sign(payload, SECRET_KEY);
+  return res.json({ _token });
 });
 
 /** POST /register: registers, logs in, and returns token.
@@ -29,9 +28,9 @@ router.post("/login", async function (req, res, next) {
 router.post("/register", async function (req, res, next) {
   const { username, password, first_name, last_name, phone } = req.body;
   User.register({ username, password, first_name, last_name, phone });
-  const payload = username;
-  const token = jwt.sign(payload, SECRET_KEY);
-  return res.send({ token });
+  const payload = { username };
+  const _token = jwt.sign(payload, SECRET_KEY);
+  return res.json({ _token });
 });
 
 
