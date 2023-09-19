acef2a3e73b63512e1fa12e171dc4fa3007c1f93
Christopher Alley <calleyviola@gmail.com>
2023-09-19 13:20:41 -0400

    Add all user routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const User = require("../models/user");
+
 const Router = require("express").Router;
 const router = new Router();
 
@@ -10,12 +13,22 @@ const router = new Router();
  *
  **/
 
+router.get("/", ensureLoggedIn, async function (req, res, next) {
+  const users = await User.all();
+  return res.json({ users });
+});
+
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+
+router.get("/:username", ensureLoggedIn, ensureCorrectUser, async function (req, res, next) {
+  const user = await User.get(req.params.username);
+  return res.json({ user });
+});
 
 
 /** GET /:username/to - get messages to user
@@ -28,6 +41,11 @@ const router = new Router();
  *
  **/
 
+router.get("/:username/to", ensureLoggedIn, ensureCorrectUser, async function (req, res, next) {
+  const messages = await User.messagesTo(req.params.username);
+  return res.json({ messages });
+});
+
 
 /** GET /:username/from - get messages from user
  *
@@ -38,5 +56,10 @@ const router = new Router();
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+router.get("/:username/from", ensureLoggedIn, ensureCorrectUser, async function (req, res, next) {
+  const messages = await User.messagesFrom(req.params.username);
+  return res.json({ messages });
+});
 
 module.exports = router;
